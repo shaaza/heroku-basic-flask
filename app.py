e24aaad3f79dd165d3002fd579e69ca258513676
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2018-01-10 00:47:08 +0530

    Add readable URLs for blog posts on Medium

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from datetime import datetime
 app = Flask(__name__)
 
@@ -13,6 +13,25 @@ def homepage():
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
+@app.route('/optimizing-sql-queries')
+def sql():
+    return redirect("https://medium.com/@shaaz_a/how-to-choose-a-table-index-for-your-sql-database-d47715a35f34", code=302)
+
+@app.route('/programming-with-purity')
+def sql():
+    return redirect("https://medium.com/@shaaz_a/methods-of-functional-programming-using-python-or-javascript-b3309bb2c807", code=302)
+
+@app.route('/architecting-a-text-adventure-game')
+def sql():
+    return redirect("https://medium.com/@shaaz_a/architecting-a-text-adventure-game-2e0dc4d49812", code=302)
+
+@app.route('/seeing-responsibilities')
+def sql():
+    return redirect("https://medium.com/@shaaz_a/latest", code=302)
+
+@app.route('/string-matching-indic-text')
+def sql():
+    return redirect("https://medium.com/@shaaz_a/latest", code=302)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
